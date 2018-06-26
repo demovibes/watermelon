51f9b4f32fc3f9b75af725f8401fb3f97a7ad474
Greg Kennedy <kennedy.greg@gmail.com>
2018-06-26 00:19:41 -0500

    Add detail pages for Artist and Song.  Simplify Index views.

--- artists/urls.py
+++ artists/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^(?P<artist_id>[0-9]+)$', views.detail, name='artists-detail'),
     url(r'^$', views.index, name='index'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,18 +1,23 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from .models import Song
 
+# General index page for a Songs request
 def index(request):
-    song_list = Song.objects.order_by('name')
+    return JsonResponse(
+        list(Song.objects.values('id','name','platform','artist')),
+        safe=False)
 
-    # put all song data into a list of dictionary items
-    song_response = [
-    {
-      'name': s.name,
-
-      'platform': s.platform_id,
-
-      'artist': s.artist.name,
-    } for s in song_list ]
-
-    return JsonResponse(song_response, safe=False)
+# Specific page of a song
+def detail(request, song_id):
+    obj = Song.objects.get(pk=song_id)
+    return JsonResponse(
+        {
+            'id': obj.id,
+            'name': obj.name,
+            'platform': obj.platform_id,
+            'artist_id': obj.artist_id,
+            'artist_name': obj.artist.name,
+        },
+        safe=False)
