22b7220f6a77b4419d779cbd9251d7b667ff5c3f
Greg Kennedy <kennedy.greg@gmail.com>
2019-06-17 02:42:04 -0500

    Move apps into subdirectory under the main project

--- watermelon/urls.py
+++ watermelon/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^songs/', include('songs.urls')),
-    url(r'^artists/', include('artists.urls')),
-    url(r'^streams/', include('streams.urls')),
+    url(r'^songs/', include('watermelon.apps.songs.urls')),
+    url(r'^artists/', include('watermelon.apps.artists.urls')),
+    url(r'^streams/', include('watermelon.apps.streams.urls')),
     url(r'^admin/', admin.site.urls),
 ]
