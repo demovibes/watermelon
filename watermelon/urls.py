e605e7952d7860ace94c61fdfb89df8f91e6f9d4
Greg Kennedy <kennedy.greg@gmail.com>
2019-06-17 02:49:28 -0500

    Rename project from watermelon to demovibes, and upgrade to Django 2.2

--- watermelon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^songs/', include('watermelon.apps.songs.urls')),
-    url(r'^artists/', include('watermelon.apps.artists.urls')),
-    url(r'^streams/', include('watermelon.apps.streams.urls')),
-    url(r'^admin/', admin.site.urls),
-]
